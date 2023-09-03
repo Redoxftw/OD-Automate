2b8af7b28d86dadbd768f8f7b197396244d04b30
BytesBlaze <8055kunalkumar@gmail.com>
2023-09-03 22:16:04 +0530

    Location

--- public/client_script.js
+++ public/client_script.js
@@ -1,74 +1,76 @@
 let regValue = document.getElementById('regsitrationInput');
-let nameValue = document.getElementById('nameInput')
+let nameValue = document.getElementById('nameInput');
 
 regValue.addEventListener('input', () => {
-    regValue.value = regValue.value.toUpperCase();
-    document.getElementById('displaystatus').innerText =""
+  regValue.value = regValue.value.toUpperCase();
+  document.getElementById('displaystatus').innerText = "";
 });
+
 nameValue.addEventListener('input', () => {
-    nameValue.value = nameValue.value.toUpperCase();
+  nameValue.value = nameValue.value.toUpperCase();
 });
+
 let verifyOldBtn = document.getElementById('old');
 
 const sentAuth = async () => {
-
-    try {
-        const res = await fetch(`/api/olduser/auth/${regValue.value}/${nameValue.value}/${lat}/${long}`);
-        const result = await res.json()
-        modifyStatus(result)
-        console.log(result)
-    } catch (error) {
-        console.error(error)
+  try {
+    if (await askForLocationPermission()) {
+      const res = await fetch(`/api/olduser/auth/${regValue.value}/${nameValue.value}/${lat}/${long}`);
+      const result = await res.json();
+      modifyStatus(result);
+      console.log(result);
+    } else {
+      // User denied location permission, display a warning
+      alert('Please enable location services in your device settings.');
     }
-}
-verifyOldBtn.addEventListener('click', sentAuth)
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+verifyOldBtn.addEventListener('click', sentAuth);
 
 const modifyStatus = (result) => {
-    if (result.status==='Verified') {
-        document.getElementById('displaystatus').innerText = `${result.status}`
-        document.getElementById('displaystatus').style.color='green';
-        
-    } else {
-        document.getElementById('displaystatus').innerText = `${result.status}`
-        document.getElementById('displaystatus').style.color='red';
-    }
-}
-const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-  let lat;
-  let long;
- 
-
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
-  function requestLocationPermission() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        function(position) {
-          // Do something with the user's location
-          console.log("Latitude: " + position.coords.latitude);
-          console.log("Longitude: " + position.coords.longitude);
-          lat=crd.latitude;
-          long=crd.longitude;
-        },
-        function(error) {
-          if (error.code === 1) {
-            // The user denied location permission
-            alert("Please enable location services in your device settings.");
-          } else {
-            // Handle other errors
-            console.error("Error getting location:", error);
-          }
-        }
-      );
-    } else {
-      alert("Geolocation is not supported in this browser.");
-    }
+  if (result.status === 'Verified') {
+    document.getElementById('displaystatus').innerText = `${result.status}`;
+    document.getElementById('displaystatus').style.color = 'green';
+  } else {
+    document.getElementById('displaystatus').innerText = `${result.status}`;
+    document.getElementById('displaystatus').style.color = 'red';
   }
-  
-  // Call the function to request location permission
-  requestLocationPermission();
-  
+};
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+let lat;
+let long;
+
+function success(pos) {
+  const crd = pos.coords;
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  lat = crd.latitude;
+  long = crd.longitude;
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function askForLocationPermission() {
+  return new Promise((resolve) => {
+    navigator.permissions.query({ name: 'geolocation' }).then((result) => {
+      if (result.state === 'granted') {
+        resolve(true);
+      } else if (result.state === 'prompt') {
+        navigator.geolocation.getCurrentPosition(success, error, options);
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+}
