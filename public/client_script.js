3cd038d9e3389ad3dbcb0b56f4e5b6f1275aec8b
BytesBlaze <8055kunalkumar@gmail.com>
2023-09-06 14:49:52 +0530

    major

--- public/client_script.js
+++ public/client_script.js
@@ -7,13 +7,12 @@ const options = {
 };
 
 function success(GeolocationPosition) {
-  user_latitude = GeolocationPosition.coords.latitude
-  user_longitude = GeolocationPosition.coords.longitude
+  user_latitude = GeolocationPosition.coords.latitude;
+  user_longitude = GeolocationPosition.coords.longitude;
 }
 
 function error(GeolocationPositionError) {
   user_location_error_code = GeolocationPositionError.message;
-  console.log(user_location_error_code)
 }
 
 const student_registration_number = document.getElementById('regsitrationInput');
@@ -21,7 +20,7 @@ const student_name = document.getElementById('nameInput');
 
 student_registration_number.addEventListener('input', () => {
   student_registration_number.value = student_registration_number.value.toUpperCase();
-  document.getElementById('displaystatus').innerText = "";
+  document.getElementById("status-container").innerHTML = "";
 });
 
 student_name.addEventListener('input', () => {
@@ -42,28 +41,28 @@ const sentAuth = async () => {
     }
   }
   else {
-    document.getElementById('displaystatus').innerText = `ERR_MSG : ${user_location_error_code}`;
-    document.getElementById('displaystatus').style.color = 'red';
-    document.getElementById('displaystatus').style.fontSize = '16px';
+    document.getElementById("status-container").innerHTML = `<div class="status-container">
+    <h1 id="displaystatus" style="color: red; height: 70px; font-size: 16px;">ERR_MSG :${user_location_error_code}</h1>
+    </div>`
   };
 }
 
 const lodaing_message = () => {
-  document.getElementById('displaystatus').innerHTML = `<img id="buffer-icon" src="https://www.msha.gov/sites/default/files/images/loading2.gif" style="
+  document.getElementById("status-container").innerHTML = `<img id="buffer-icon" src="https://www.msha.gov/sites/default/files/images/loading2.gif" style="
   mix-blend-mode: color-burn;
   width: 70px;">`;
   setTimeout(sentAuth, 2000);
-}
+} 
 verifyButton.addEventListener('click', lodaing_message);
 
-
 const modifyStatus = (result) => {
-  document.getElementById('buffer-icon').style.visibility = "hidden";
   if (result.status === 'Verified') {
-    document.getElementById('displaystatus').innerText = `${result.status}`;
-    document.getElementById('displaystatus').style.color = 'green';
+    document.getElementById("status-container").innerHTML = `<div class="status-container">
+    <h1 id="displaystatus" style="color: green; height: 70px; font-size: 28px;">Verified</h1>
+    </div>`;
   } else {
-    document.getElementById('displaystatus').innerText = `${result.status}`;
-    document.getElementById('displaystatus').style.color = 'red';
+    document.getElementById("status-container").innerHTML = `<div class="status-container">
+    <h1 id="displaystatus" style="color: red; height: 70px; font-size: 28px;">Not Found</h1>
+    </div>`;
   }
 };
